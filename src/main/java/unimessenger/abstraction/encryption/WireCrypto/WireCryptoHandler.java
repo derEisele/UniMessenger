0f757c365fd755552702545dbd03267f35913a7b
Fi0x <felpech@aol.com>
2020-11-24 22:22:48 +0100

    Started filling encryption method
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/unimessenger/abstraction/encryption/WireCrypto/WireCryptoHandler.java
+++ src/main/java/unimessenger/abstraction/encryption/WireCrypto/WireCryptoHandler.java
@@ -1,10 +1,16 @@
 package unimessenger.abstraction.encryption.WireCrypto;
 
+import com.wire.bots.cryptobox.CryptoBox;
 import com.wire.bots.cryptobox.CryptoException;
 import com.wire.bots.cryptobox.PreKey;
+import unimessenger.userinteraction.Outputs;
+
+import java.util.Base64;
 
 public class WireCryptoHandler
 {
+    static CryptoBox box;//TODO: Get the right box
+
     public static Prekey[] generatePreKeys(int start, int count)
     {
         Prekey[] Keys = new Prekey[count];
@@ -43,10 +49,26 @@ public class WireCryptoHandler
 
     }
 
-    public static String encrypt(String userID, String clientID, Prekey pk, String content)
+    public static String encrypt(String userID, String clientID, Prekey pk, String msg)
     {
-        //TODO: Return "OTR Content"
-        return "OTR Content";
+        byte[] content = getByteStreamFromMessage(msg);
+        String id = userID + "_" + clientID;
+        PreKey key = new PreKey(pk.getID(), Base64.getDecoder().decode(pk.getKey()));
+        byte[] cypher = new byte[0];
+        try
+        {
+            cypher = box.encryptFromPreKeys(id, key, content);
+        } catch(CryptoException ignored)
+        {
+            Outputs.printError("Encrypting message failed");
+        }
+        return Base64.getEncoder().encodeToString(cypher);
+    }
+
+    private static byte[] getByteStreamFromMessage(String message)
+    {
+        //TODO: Transform the message String to an IGeneric message from xenon and return it as byte[]
+        return null;
     }
 
     public static void cleanUp()
